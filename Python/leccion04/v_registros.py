0a7e6c15948ebbf73bbd663294c711c2f91a78af
Gastón Exequiel Garay <111871436+gasexgg@users.noreply.github.com>
2023-05-23 23:28:55 -0300

    v_registros.py

---
+++ Python/leccion04/v_registros.py
@@ -0,0 +1,20 @@
+#PARA VARIOS REGISTROS
+
+import psycopg2 # Esto es para poder conectarnos a Postgre
+
+conexion = psycopg2.connect(user='postgres', password='3875622315', host='127.0.0.1', port='5432', database='test bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona IN %s' #Placeholder
+            entrada = input('Digite los id_persona a buscar(separados por coma): ')
+            llaves_primarias = (tuple(entrada.split(', ')),) #pasando en tuplas y conversion de valores
+            cursor.execute(sentencia, llaves_primarias) # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchall() # Recuperamos todos los registros que serán una lista
+            for registro in registros:
+                print(registro)
+
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
